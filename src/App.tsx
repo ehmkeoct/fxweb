d06b164c334955a9e3688b7ddcec0eb5a9628c84
ehmkeoct <cehmke@allmysons.com>
2024-06-19 17:22:03 -0500

    start up

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Login from './components/Login/Login'
+import Dashboard from './components/Dashboard/Dashboard'
 
 function App() {
+
   return (
     <div className="App">
+      
       <header className="App-header">
-        <p>
-          This is the test website for FX Web
-        </p>
-
+        <div className ="header-content">
+          <p>
+            This is the skellington website for FX Web
+          </p>
+        </div>
       </header>
+      <Login />
+      <Dashboard/>
       <body className="App-body">
-      <img src={logo} className="App-logo" alt="logo" />
-      <a
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
           className="App-link"
           href="https://github.com/ehmkeoct/fxweb"
           target="_blank"
           rel="noopener noreferrer"
         >
           Project GitHub
+        </a>
+        <a
+          className="App-link"
+          href="https://us-east-2.console.aws.amazon.com/amplify/apps/dydv7y5329qud/overview"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ampllify
         </a>
       </body>
     </div>
